458f6fa1a28d386aed09d66026c0050de347fd21
rotsenk <stanley.95@hotmail.com>
2024-07-25 16:45:56 -0600

    Express configuration

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('hola mundo!')
+const express = require('express');
+
+// crear el servidor de express
+const app = express();// express como una función
+
+
+// rutas
+app.get('/', (req, res) => {
+    res.json({
+        ok: true
+    })
+});
+
+
+
+// escuchar peticiones
+// no poner puerto 3000, sino diferente 3001, 4000
+app.listen(4000, () => {
+    console.log(`servidor corriendo en puerto ${4000}`)
+})
