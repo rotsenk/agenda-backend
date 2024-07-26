19e0a1f7ad07b9e2952820fb869b007313c54fd7
rotsenk <stanley.95@hotmail.com>
2024-07-26 11:39:37 -0600

    creating environment variables and public folder

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 const express = require('express');
+require('dotenv').config();
 
 // crear el servidor de express
 const app = express();// express como una función
 
+// directorio público
+app.use( express.static('public') );
 
 // rutas
-app.get('/', (req, res) => {
-    res.json({
-        ok: true
-    })
-});
-
+// app.get('/', (req, res) => {
+//     res.json({
+//         ok: true
+//     })
+// });
 
 
 // escuchar peticiones
 // no poner puerto 3000, sino diferente 3001, 4000
-app.listen(4000, () => {
-    console.log(`servidor corriendo en puerto ${4000}`)
+app.listen( process.env.PORT, () => {
+    console.log(`servidor corriendo en puerto ${ process.env.PORT }`);
 })
