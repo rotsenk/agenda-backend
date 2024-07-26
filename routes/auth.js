d0e65277cd56394998d1ada2c070ad7f72e0c587
rotsenk <stanley.95@hotmail.com>
2024-07-26 15:35:39 -0600

    Remove, create, and login endpoints

--- routes/auth.js
+++ routes/auth.js
@@ -5,12 +5,17 @@
 const { Router } = require('express');
 const router = Router();
 
+const { crearUsuario, loginUsuario, revalidarToken } = require('../controllers/auth');
 
-router.get('/', (req, res) => {
-    res.json({
-        ok: true
-    })
-});
+// e n d p o i n t s
+// para nuevos usuarios
+router.post( '/new', crearUsuario );
+
+// posteo directo al /auth
+router.post( '/', loginUsuario );
+
+// del token autenticar de forma pasova
+router.get( '/renew', revalidarToken );
 
 
 module.exports = router;
