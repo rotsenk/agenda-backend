c039e6492ac9950cd150531415b46f7e83524774
rotsenk <stanley.95@hotmail.com>
2024-07-29 10:53:36 -0600

    express validator

--- routes/auth.js
+++ routes/auth.js
@@ -3,19 +3,35 @@
  */
 
 const { Router } = require('express');
+const { check } = require('express-validator');
 const router = Router();
 
 const { crearUsuario, loginUsuario, revalidarToken } = require('../controllers/auth');
 
 // e n d p o i n t s
 // para nuevos usuarios
-router.post( '/new', crearUsuario );
+router.post(
+    '/new',
+    [// middlewares
+        check('name', 'El nombre es obligatorio').not().isEmpty(),
+        check('email', 'El email es obligatorio').isEmail(),
+        check('password', 'El password debe de ser de 6 caracteres').isLength({ min:6 })
+    ],
+    crearUsuario
+);
 
 // posteo directo al /auth para login
-router.post( '/', loginUsuario );
+router.post(
+    '/',
+    [// middlewares
+        check('email', 'El email es obligatorio').isEmail(),
+        check('password', 'El password debe de ser de 6 caracteres').isLength({ min:6 })
+    ],
+    loginUsuario
+);
 
 // del token autenticar de forma pasiva
-router.get( '/renew', revalidarToken );
+router.get('/renew', revalidarToken);
 
 
 module.exports = router;
