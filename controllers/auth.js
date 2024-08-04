09299776ea4f658bebfac812d901be21c8081ea4
rotsenk <stanley.95@hotmail.com>
2024-08-04 12:50:55 -0600

    encrypting password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,5 @@
 const { response } = require('express');
-const { validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
 const Usuario = require('../models/Usuario');
 
 const crearUsuario = async ( req, res = response ) => {
@@ -18,6 +18,11 @@ const crearUsuario = async ( req, res = response ) => {
         }
         
         usuario = new Usuario( req.body );
+
+        // encriptar contraseña
+        const salt = bcrypt.genSaltSync();
+        usuario.password = bcrypt.hashSync( password, salt );
+
         await usuario.save();
     
         res.status(201).json({
