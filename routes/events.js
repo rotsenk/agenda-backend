15613eaa4ca453b8ccf64a7392904bf0ea0bb227
rotsenk <stanley.95@hotmail.com>
2025-02-20 10:55:32 -0600

    modelo de eventos

--- routes/events.js
+++ routes/events.js
@@ -2,7 +2,6 @@
  * Event Routes
  * /api/events
  */
-
 // crear todo el CRUD
 const { Router } = require('express');
 const { validarJWT } = require('../middlewares/validar-jwt');
@@ -10,17 +9,18 @@ const { getEventos, crearEvento, actualizarEvento, eliminarEvento } = require('.
 const router = Router();
 
 // todas tienen que pasar por la validación jwt
+router.use( validarJWT );
 
 // Obtener eventos
-router.get('/', validarJWT, getEventos);
+router.get('/', getEventos);
 
 // Crear un nuevo evento
-router.post('/', validarJWT, crearEvento);
+router.post('/', crearEvento);
 
 // Actualizar evento
-router.put('/:id', validarJWT, actualizarEvento);
+router.put('/:id', actualizarEvento);
 
 // Actualizar evento
-router.delete('/:id', validarJWT, eliminarEvento);
+router.delete('/:id', eliminarEvento);
 
 module.exports = router;
